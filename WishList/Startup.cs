8fcf1313b5e38afe7e8ea4ddbf7a290d54466002
Preeti Poddar <preeti.poddar@somaiya.edu>
2021-09-25 20:34:17 +0530

    #1

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -32,8 +32,7 @@ namespace WishList
                 app.UseExceptionHandler("/Home/Error");
             }
             app.UseRouting();
-            app.UseEndpoints(
-                endpoints => { endpoints.MapDefaultControllerRoute(); });
+            app.UseEndpoints(endpoints => { endpoints.MapDefaultControllerRoute(); });
         }
     }
 }
